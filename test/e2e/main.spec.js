a2b825d5710e87ae85508a132a02c711d9b448d7
Ye Wang <wangye@nih.gov>
2020-06-11 15:09:47 -0400

    Revert "fixed tests"
    This reverts commit 97d50b722afcc9dda0dfb3aa0e9fd2fe8930f335.

--- test/e2e/main.spec.js
+++ test/e2e/main.spec.js
@@ -1,148 +1,9 @@
 'use strict';
 
-const { $, browser } = require("protractor");
-
 const os = require("os"),
   EC = protractor.ExpectedConditions;
 
-describe('FHIR Questionnaire Viewer', function() {
-
-  describe('URLs provided on page', function() {
-    beforeAll(function () {
-      setAngularSite(false);
-    });
-  
-    beforeEach(function () {
-      browser.get('/');
-    });
-    
-    it('should load a Questionnaire without resource pakcage', function () {
-      let urlQ = element(by.id('urlQuestionnaire')),
-      urlP =  element(by.id('urlPackage')),
-      firstItem =  element(by.id('/q1/1')),
-      btn = element(by.id('load'));
-  
-      urlQ.clear();
-      urlQ.sendKeys("http://localhost:8080/questionnaire-use-package.json");
-      btn.click();
-      browser.wait(EC.visibilityOf(firstItem));
-      firstItem.click();
-      // expect(firstItem.searchResults.isPresent()).toBeFalsy();
-      firstItem.sendKeys(protractor.Key.ARROW_DOWN);
-      firstItem.sendKeys(protractor.Key.TAB);
-      expect(firstItem.getAttribute('value')).toBe('');
-  
-    });
-  
-  
-    it('should load a Questionnaire with a resource pakcage', function () {
-      let urlQ = element(by.id('urlQuestionnaire')),
-      urlP =  element(by.id('urlPackage')),
-      firstItem =  element(by.id('/q1/1')),
-      secondItem =  element(by.id('/q2/1')),
-      thirdItem =  element(by.id('/q3/1')),
-      btn = element(by.id('load'));
-  
-      urlQ.clear();
-      urlQ.sendKeys("http://localhost:8080/questionnaire-use-package.json");
-      urlP.clear();
-      urlP.sendKeys("http://localhost:8080/package.json.tgz");
-      btn.click();
-      browser.wait(EC.visibilityOf(firstItem));
-  
-      firstItem.click();
-      firstItem.sendKeys(protractor.Key.ARROW_DOWN);
-      firstItem.sendKeys(protractor.Key.TAB);
-      expect(firstItem.getAttribute('value')).toBe("Cholesterol [Moles/volume] in Serum or Plasma");
-  
-      secondItem.click();
-      secondItem.sendKeys(protractor.Key.ARROW_DOWN);
-      secondItem.sendKeys(protractor.Key.ARROW_DOWN);
-      secondItem.sendKeys(protractor.Key.TAB);
-      expect(secondItem.getAttribute('value')).toBe("Cholesterol/Triglyceride [Mass Ratio] in Serum or Plasma");
-  
-      thirdItem.click();
-      thirdItem.sendKeys(protractor.Key.ARROW_DOWN);
-      thirdItem.sendKeys(protractor.Key.ARROW_DOWN);
-      thirdItem.sendKeys(protractor.Key.ARROW_DOWN);
-      thirdItem.sendKeys(protractor.Key.TAB);
-      expect(thirdItem.getAttribute('value')).toBe("Cholesterol/Triglyceride [Mass Ratio] in Serum or Plasma");
-  
-    });
-  
-  });
-
-  
-  describe('URLs provided as url parameters', function() {
-    beforeAll(function () {
-      setAngularSite(false);
-    });
-  
-    beforeEach(function () {
-      browser.get('/');
-    });
-  
-  
-    it('should load a Questionnaire without resource pakcage', function () {
-      let url = browser.baseUrl + '/?q=' + browser.baseUrl + '/questionnaire-use-package.json';
-      console.log(url);
-      browser.get(url);
-
-      let firstItem =  element(by.id('/q1/1')),
-      inputPanel = element(by.id('form-input'));
-  
-      browser.wait(EC.visibilityOf(firstItem));
-      firstItem.click();
-      // expect(firstItem.searchResults.isPresent()).toBeFalsy();
-      firstItem.sendKeys(protractor.Key.ARROW_DOWN);
-      firstItem.sendKeys(protractor.Key.TAB);
-      expect(firstItem.getAttribute('value')).toBe('');
-  
-      // input panel is not shown
-      expect(inputPanel.isDisplayed()).toBeFalsy();
-    });
-  
-  
-    it('should load a Questionnaire with a resource pakcage', function () {
-      let url = browser.baseUrl + '/?q=' + browser.baseUrl + '/questionnaire-use-package.json' + '&p=' + browser.baseUrl + '/package.json.tgz';
-      console.log(url);
-      browser.get(url);
-
-      let firstItem =  element(by.id('/q1/1')),
-      secondItem =  element(by.id('/q2/1')),
-      thirdItem =  element(by.id('/q3/1')),
-      inputPanel = element(by.id('form-input'));
-      
-      browser.wait(EC.visibilityOf(firstItem));
-  
-      firstItem.click();
-      firstItem.sendKeys(protractor.Key.ARROW_DOWN);
-      firstItem.sendKeys(protractor.Key.TAB);
-      expect(firstItem.getAttribute('value')).toBe("Cholesterol [Moles/volume] in Serum or Plasma");
-  
-      secondItem.click();
-      secondItem.sendKeys(protractor.Key.ARROW_DOWN);
-      secondItem.sendKeys(protractor.Key.ARROW_DOWN);
-      secondItem.sendKeys(protractor.Key.TAB);
-      expect(secondItem.getAttribute('value')).toBe("Cholesterol/Triglyceride [Mass Ratio] in Serum or Plasma");
-  
-      thirdItem.click();
-      thirdItem.sendKeys(protractor.Key.ARROW_DOWN);
-      thirdItem.sendKeys(protractor.Key.ARROW_DOWN);
-      thirdItem.sendKeys(protractor.Key.ARROW_DOWN);
-      thirdItem.sendKeys(protractor.Key.TAB);
-      expect(thirdItem.getAttribute('value')).toBe("Cholesterol/Triglyceride [Mass Ratio] in Serum or Plasma");
-
-      // input panel is not shown
-      expect(inputPanel.isDisplayed()).toBeFalsy();
-
-    });
-  
-  });
-  
-});
-
-describe('URLs provided on page', function() {
+describe('Research Data Finder', function() {
   beforeAll(function () {
     setAngularSite(false);
   });
@@ -151,83 +12,48 @@ describe('URLs provided on page', function() {
     browser.get('/');
   });
 
-  // describe('guessFHIRVersion', function() {
-  //   it('should recognize an STU3 Questionnaire with "option"', function(done) {
-  //     $.get('/test/data/STU3/weightHeightQuestionnaire.json', function z(whQ) {
-  //       // Remove the meta.profile so we can test whether the guess function can
-  //       // correctly guess based on structure only.
-  //       delete whQ.meta;
-  //       var fhirVersion = LForms.Util.guessFHIRVersion(whQ);
-  //       assert.equal(fhirVersion, 'STU3');
-  //       done();
-  //     });
-  //   });
-  
-  //   it('should recognize an R4 Questionnaire with "option"', function(done) {
-  //     $.get('/test/data/R4/weightHeightQuestionnaire.json', function z(whQ) {
-  //       // Remove the meta.profile so we can test whether the guess function can
-  //       // correctly guess based on structure only.
-  //       delete whQ.meta;
-  //       var fhirVersion = LForms.Util.guessFHIRVersion(whQ);
-  //       assert.equal(fhirVersion, 'R4');
-  //       done();
-  //     });
-  //   });
+  // TODO: Temporarily commented because it does not work yet
+  // it('should load observations filtered by category', function () {
+  //   $('#limit1').click();
+  //   $('#load').click();
+  //   browser.wait(EC.visibilityOf($('#results')));
   // });
-  
 
-  it('should load a Questionnaire without resource pakcage', function () {
-    let urlQ = element(by.id('urlQuestionnaire')),
-    urlP =  element(by.id('urlPackage')),
-    firstItem =  element(by.id('/q1/1')),
-    btn = element(by.id('load'));
-
-    urlQ.clear();
-    urlQ.sendKeys("http://localhost:8080/questionnaire-use-package.json");
-    btn.click();
-    browser.wait(EC.visibilityOf(firstItem));
-    firstItem.click();
-    // expect(firstItem.searchResults.isPresent()).toBeFalsy();
-    firstItem.sendKeys(protractor.Key.ARROW_DOWN);
-    firstItem.sendKeys(protractor.Key.TAB);
-    expect(firstItem.getAttribute('value')).toBe('');
-
+  it('should load observations filtered by tests', function () {
+    // TODO: Temporarily commented because it does not work yet
+    // $('#limit2').click();
+    $('#load').click();
+    browser.wait(EC.visibilityOf($('#results')));
   });
 
+  it('should download observations', function () {
+    $('#load').click();
+    browser.wait(EC.visibilityOf($('#results')));
 
-  it('should load a Questionnaire with a resource pakcage', function () {
-    let urlQ = element(by.id('urlQuestionnaire')),
-    urlP =  element(by.id('urlPackage')),
-    firstItem =  element(by.id('/q1/1')),
-    secondItem =  element(by.id('/q2/1')),
-    thirdItem =  element(by.id('/q3/1')),
-    btn = element(by.id('load'));
+    const filename = os.tmpdir() + '/observations.csv';
+    const fs = require('fs');
 
-    urlQ.clear();
-    urlQ.sendKeys("http://localhost:8080/questionnaire-use-package.json");
-    urlP.clear();
-    urlP.sendKeys("http://localhost:8080/package.json.tgz");
-    btn.click();
-    browser.wait(EC.visibilityOf(firstItem));
+    if (fs.existsSync(filename)) {
+      // Make sure the browser doesn't have to rename the download.
+      fs.unlinkSync(filename);
+    }
 
-    firstItem.click();
-    firstItem.sendKeys(protractor.Key.ARROW_DOWN);
-    firstItem.sendKeys(protractor.Key.TAB);
-    expect(firstItem.getAttribute('value')).toBe("Cholesterol [Moles/volume] in Serum or Plasma");
+    $('#download').click();
 
-    secondItem.click();
-    secondItem.sendKeys(protractor.Key.ARROW_DOWN);
-    secondItem.sendKeys(protractor.Key.ARROW_DOWN);
-    secondItem.sendKeys(protractor.Key.TAB);
-    expect(secondItem.getAttribute('value')).toBe("Cholesterol/Triglyceride [Mass Ratio] in Serum or Plasma");
+    browser.driver.wait(function() {
+      // Wait until the file has been downloaded.
+      return fs.existsSync(filename);
+    }, 30000).then(function() {
+      // Checks CSV file structure: the file has columns and all lines have the same number of cells.
+      const cellsInRowCount = fs.readFileSync(filename, {encoding: 'utf8'})
+          .replace(/""/g, '').replace(/"[^"]*"/g, '').split('\n')
+          .map(line => line.split(',').length),
+        columnsCount = cellsInRowCount[0];
 
-    thirdItem.click();
-    thirdItem.sendKeys(protractor.Key.ARROW_DOWN);
-    thirdItem.sendKeys(protractor.Key.ARROW_DOWN);
-    thirdItem.sendKeys(protractor.Key.ARROW_DOWN);
-    thirdItem.sendKeys(protractor.Key.TAB);
-    expect(thirdItem.getAttribute('value')).toBe("Cholesterol/Triglyceride [Mass Ratio] in Serum or Plasma");
+      expect(columnsCount > 0 && cellsInRowCount.every(cellsCount => cellsCount === columnsCount)).toBe(true);
 
+      // Cleanup
+      fs.unlinkSync(filename);
+    });
   });
-
 });
--- webpack.development.js
+++ webpack.development.js
@@ -17,7 +17,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: ['./public', './test/data/'],
+    contentBase: './public',
     host: '0.0.0.0',
     //host: 'localhost',
     port: port,
