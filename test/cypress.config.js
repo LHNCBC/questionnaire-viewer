9fc9828af7fad6bb6821665187f255953efd3be0
Paul Lynch <plynch@mail.nih.gov>
2023-05-08 16:39:34 -0400

    Revert "Revert "Merge pull request #13 in LFOR/questionnaire-viewer from update_node to master""
    This reverts commit 46d1e5b7e50381be37daf4056acb5d9f10f0330b.

--- test/cypress.config.js
+++ test/cypress.config.js
@@ -1,11 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-    e2e: {
-        setupNodeEvents(on, config) {
-            // implement node event listeners here
-        },
-        baseUrl: 'http://localhost:4029/',
-        specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}'
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
+    baseUrl: 'http://localhost:'+require('../package.json').config.testServerPort+'/',
+    specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}'
+  },
+  video: false
 });
